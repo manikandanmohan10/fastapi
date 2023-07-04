ac851335dbafe51dc3da5dba41bbcd30a42b8f08
manikandanmohan10 <manikandan.mohan@softsuave.com>
2023-07-04 11:25:17 +0530

    Mongo

--- routes/route.py
+++ routes/route.py
@@ -1,13 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from models.user import User
 from config.database import collection_name
-from schema.schemas import list_serial
+from schema.schemas import list_serial, individual_serial
 from bson import ObjectId
 
 router = APIRouter()
 
-@router.get("/")
-async def get_users():
-    users = list_serial(collection_name.find())
+@router.get("/users-list/{id}")
+def get_users(id: str):
+    if not id:
+        return "Params not found"
+    if id == 'all':
+        users = list_serial(collection_name.find())
+        return {"data": users}
     
-    return users
+    users = individual_serial(collection_name.find_one({"_id":ObjectId(id)}))
+    if not users:
+        return 404
+    return {'data': users}
+
+@router.post("/register")
+def register(data: User):
+    collection_name.insert_one(dict(data))
+    
+    return 200
